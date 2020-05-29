74f6b883e623a8c7b370a5ee12df8eceea88830c
Carrie <kaka784804@gmail.com>
2020-05-29 12:48:05 +0800

    first commit

---
+++ test/sum.test.js
@@ -0,0 +1,25 @@
+﻿var expect = require('chai').expect;
+var method = require("../lib/sum.js");
+
+describe("測試加總函數", function() {
+    /* Returns 0 if no argument is passed */
+    it("Expect return 0 if no argument.", function(){
+        var argument = [];
+        expect(method.sum(argument)).to.be.equal(0);
+    })
+
+    /* Return false if argument is not array */
+    it("without array.", function(){
+        var arguments = 5;
+        expect(method.sum(arguments)).to.be.false;
+    })
+
+    /* Computes and returns the sum of its arguments provided all are numbers */
+    it("Expect returns the sum if arguments all are numbers.", function(){
+        var arguments = [2,3,5];
+        expect(method.sum(arguments)).to.be.equal(10);
+    })
+
+    /** 測試如果不是數字時，回傳Exception?? */
+  }
+)
---
+++ test/tool.test.js
@@ -0,0 +1,35 @@
+﻿var expect = require('chai').expect;
+var method = require("../lib/tool.js");
+
+describe("判斷字串是否為空字串、空白、未定義、null", function() {
+    it("Expect return false when the name is not empty.", function(){
+        var name = "Carrie";
+        expect(method.textIsEmpty(name)).to.be.false;
+    })
+
+    it("Expect return true when the name is empty.", function(){
+        var name = "";
+        expect(method.textIsEmpty(name)).to.be.true;
+    })
+
+    it("Expect return true when the name is null.", function(){
+        var name = null;
+        expect(method.textIsEmpty(name)).to.be.true;
+    })
+  }
+)
+
+describe("數字加上千分位逗號", function() {
+    it("Expect return 1,00 when the number is not 1000", function(){
+        var value = "1,000";
+        var number = 1000;
+
+        expect(method.numberWithCommas(number)).to.be.equal(value);
+    })
+
+    it("Expect the number will return string", function(){
+        var number = 1000;
+        expect(method.numberWithCommas(number)).to.be.a('string');
+    })
+  }
+)
